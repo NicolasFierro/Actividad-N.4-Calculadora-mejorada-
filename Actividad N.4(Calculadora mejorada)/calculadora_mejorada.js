f26979e5c120f4d7ce40424eb7dd28705a0be6cf
NicolasFierro <129073279+NicolasFierro@users.noreply.github.com>
2023-11-15 11:57:07 -0500

    Correccion (Comentado)

--- Actividad N.4(Calculadora mejorada)/calculadora_mejorada.js
+++ Actividad N.4(Calculadora mejorada)/calculadora_mejorada.js
@@ -1,61 +1,83 @@
-function sumar(){
-    let n1=parseInt(document.getElementById("n1").value)
-    let n2=parseInt(document.getElementById("n2").value)
-    resultado=n1+n2;
-    document.write("el valor de la suma seria "+resultado)
+// Función para sumar dos números ingresados por el usuario
+function sumar() {
+    // Obtener los valores de los números desde elementos HTML
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+
+    // Realizar la operación de suma
+    let resultado = n1 + n2;
+
+    // Mostrar el resultado en la página
+    document.write("el valor de la suma sería " + resultado);
 }
 
-
-function restar(){
-    let n1=parseInt(document.getElementById("n1").value)
-    let n2=parseInt(document.getElementById("n2").value)
-    resultado=n1-n2;
-    document.write("el valor de la resta seria "+resultado)
+// Función para restar dos números ingresados por el usuario
+function restar() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let resultado = n1 - n2;
+    document.write("el valor de la resta sería " + resultado);
 }
 
-
-function multiplicar(){
-    let n1=parseInt(document.getElementById("n1").value)
-    let n2=parseInt(document.getElementById("n2").value)
-    resultado=n1*n2;
-    document.write("el valor de la multiplicación seria "+resultado)
+// Función para multiplicar dos números ingresados por el usuario
+function multiplicar() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let resultado = n1 * n2;
+    document.write("el valor de la multiplicación sería " + resultado);
 }
 
-
-function dividir(){
-    let n1=parseInt(document.getElementById("n1").value)
-    let n2=parseInt(document.getElementById("n2").value)
-    resultado=n1/n2;
-    document.write("el valor de la división seria "+resultado)
+// Función para dividir dos números ingresados por el usuario
+function dividir() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    
+    // Validar la división por cero
+    if (n2 !== 0) {
+        let resultado = n1 / n2;
+        document.write("el valor de la división sería " + resultado);
+    } else {
+        document.write("No es posible dividir por cero.");
+    }
 }
 
-
-function potencia(){
-    let n1=parseInt(document.getElementById("n1").value)
-    resultado=n1*n1;
-    document.write("el valor de la potencia seria "+resultado)
+// Función para calcular la potencia (cuadrado) de un número ingresado
+function potencia() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let resultado = n1 * n1;
+    document.write("el valor de la potencia sería " + resultado);
 }
-function mostrar(){
-    let n1=parseInt(document.getElementById("n1").value)
-    let n2=parseInt(document.getElementById("n2").value)
-    let opc =parseInt(document.getElementById("opc").value)
-    switch(opc){
+
+// Función principal que determina la operación a realizar según la opción seleccionada
+function mostrar() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let opc = parseInt(document.getElementById("opc").value);
+
+    // Utilizar un switch para determinar la operación a realizar
+    switch (opc) {
         case 1:
-            sumar(n1+n2)
+            // Llamar a la función sumar con los valores n1 y n2
+            sumar();
             break;
         case 2:
-            restar()
+            // Llamar a la función restar
+            restar();
             break;
         case 3:
-            multiplicar()
+            // Llamar a la función multiplicar
+            multiplicar();
             break;
         case 4:
-            dividir()
+            // Llamar a la función dividir
+            dividir();
             break;
         case 5:
-            potencia()
+            // Llamar a la función potencia
+            potencia();
             break;
         default:
-            alert("La opción no es valida")
+            // Mostrar una alerta si la opción no es válida
+            alert("La opción no es válida");
     }
 }
